285aa1c8ccf2c8516318d3c333f0be243622955c
Renan Damascena Pereira <renandamascena@gmail.com>
2023-01-19 16:45:23 -0300

    login em desenvolvimento

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import joi from "joi"
 import bcrypt from "bcrypt"
 import { v4 as uuid} from "uuid"
+import Joi from "joi"
 
 dotenv.config()
 
@@ -22,27 +23,38 @@ try {
 }
 
 db = mongoClient.db()
-const usersCollection = db.collection("users")
-const informationsColection = db.collection("informations")
+
 
 
 app.post("/users", async (req,res) =>{
+
     const {name, email, password} = req.body
 
+    const userSchema = Joi.object({
+        name:Joi.string().min(3),
+        email:Joi.string().email(),
+        password:Joi.string().min(6)
+    })
 
-    
+    const validation = userSchema.validate({name, email, password}, { abortEarly: false })
+
+    if(validation.error)return res.status(422).send(validation.error.details)
+
     try {
+        const emailExists = await db.collection("users").findOne({name})
 
-        const emailExists = usersCollection.find({email})
+        if(emailExists)return res.status(409).send("Dados indisponíveis")
 
-        if(emailExists){return res.status(409).send("Dados indisponíveis")}
+        const hashPassword = bcrypt.hashSync(password,10)
 
-        await usersCollection.insertOne({ name, email, password })
+        await db.collection("users").insertOne({name, email, password:hashPassword})
+
+        res.status(201).send("ok")
 
     } catch (error) {
         console.log(error.message)
     }
-    res.send("ok")
+   
 })
 
 
@@ -56,8 +68,45 @@ app.get("/users", async (req,res) => {
     }
 })
 
+app.post("/sign-in", async (req,res) => {
+
+    const {email, password} = req.body
+
+    console.log(req.body)
+
+
+    try {
+        const user = await db.collection("users").findOne({email})
+
+        const confirmPassword = bcrypt.compareSync(password,user.password)
+
+
+        if(user && confirmPassword){
+
+            const token = uuid()
+
+            await db.collection("sessions").insertOne({
+                id: user._id,
+                token
+            })
+
+            return res.send(token)
+
+        }
+        else{return res.status(422).send("Dados incorretos")}
+
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+app.post("/informations", async (req,res) => {
+
+})
+
 app.get("/informations", async (req,res) => {
-    
+
 })
 
 const PORT = 5000
